d465128e4117d5154b53e60d4e5394cde2e72488
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2022-11-16 23:25:53 +0700

    update route for login

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('comingsoon', 'comingsoon.comingsoon')->name('comingsoon');
 Route::view('landing-page', 'pages.landing-page')->name('landing-page');
-Route::view('login-bs-validation', 'authentication.login-bs-validation')->name('login-bs-validation');
+Route::prefix('authentication')->group(function () {
+    Route::view('login-bs-validation', 'authentication.login-bs-validation')->name('login-bs-validation');
+});
