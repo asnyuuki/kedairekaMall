46efc446184da454655aa2d97f8a5bbd94d287ed
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2022-11-16 23:28:34 +0700

    update route for index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
+Route::get('/', function () {
+    return redirect()->route('index');
+})->name('/');
 Route::view('comingsoon', 'comingsoon.comingsoon')->name('comingsoon');
 Route::view('landing-page', 'pages.landing-page')->name('landing-page');
 Route::prefix('authentication')->group(function () {
