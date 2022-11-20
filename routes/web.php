d420ad0c4443ee6a8ba0376841ab1cc0ebc1a7ac
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2022-11-20 16:45:16 +0700

    update route for language

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return redirect()->route('index');
 })->name('/');
-
-//Language Change
-Route::get('lang/{locale}', function ($locale) {
-    if (! in_array($locale, ['en', 'id'])) {
-        abort(400);
-    }   
-    Session()->put('locale', $locale);
-    Session::get('locale');
-    return redirect()->back();
-})->name('lang');
 
 //dashboard
 Route::prefix('dashboard')->group(function () {
@@ -24,7 +13,16 @@ Route::prefix('dashboard')->group(function () {
 });
 
 Route::view('comingsoon', 'comingsoon.comingsoon')->name('comingsoon');
-// Route::view('landingpage', 'pages.landingpage')->name('landingpage');
+Route::view('faq', 'apps.faq')->name('faq');
+
+Route::prefix('others')->group(function () {
+    Route::view('400', 'errors.400')->name('error-400');
+    Route::view('401', 'errors.401')->name('error-401');
+    Route::view('403', 'errors.403')->name('error-403');
+    Route::view('404', 'errors.404')->name('error-404');
+    Route::view('500', 'errors.500')->name('error-500');
+    Route::view('503', 'errors.503')->name('error-503');
+});
 
 Route::prefix('authentication')->group(function () {
     Route::view('login', 'authentication.login')->name('login');
@@ -32,3 +30,16 @@ Route::prefix('authentication')->group(function () {
     Route::view('forget-password', 'authentication.forget-password')->name('forget-password');
     Route::view('reset-password', 'authentication.reset-password')->name('reset-password');
 });
+
+Route::prefix('users')->group(function () {
+    Route::view('user-profile', 'apps.user-profile')->name('user-profile');
+    Route::view('edit-profile', 'apps.edit-profile')->name('edit-profile');
+    Route::view('user-cards', 'apps.user-cards')->name('user-cards');
+});
+
+Route::view('bookmark', 'apps.bookmark')->name('bookmark');
+Route::view('contacts', 'apps.contacts')->name('contacts');
+Route::view('task', 'apps.task')->name('task');
+Route::view('calendar-basic', 'apps.calendar-basic')->name('calendar-basic');
+Route::view('social-app', 'apps.social-app')->name('social-app');
+Route::view('search', 'apps.search')->name('search');
